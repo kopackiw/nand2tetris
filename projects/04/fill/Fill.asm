4183d4c48ee7b62996a2e461ae0619cf0ecf85dc
kopackiw <26244440+kopackiw@users.noreply.github.com>
2022-09-01 00:19:55 +0200

    implement filling the screen in assembly

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,69 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(START)
+
+(CALCULATE_MAX_SCREEN_OFFSET)
+    @8191 // 256 rows with 32 registers
+    D=A
+    @screen_max
+    M=D
+    @SCREEN
+    D=A
+    @screen_max
+    M=M+D
+
+(CHECK_FOR_KEY_PRESS)
+    @KBD
+    D=M
+    @FILL_WHITE
+    D;JEQ
+    @FILL_BLACK
+    D;JGT
+
+(FILL_WHITE)
+    @color
+    M=0
+    @INIT_ITERATOR
+    D;JEQ
+
+(FILL_BLACK)
+    @color
+    M=-1
+    @INIT_ITERATOR
+    D;JEQ
+
+(INIT_ITERATOR)
+    @screen_max
+    D=M
+    @iterator
+    M=D
+
+(ITERATE)
+(CHECK_ITERATOR_BOUNDS)
+    @iterator
+    D=M
+    @SCREEN
+    D=D-A
+    @END_ITERATION
+    D;JLT
+
+(FILL_REGISTER)
+    @color
+    D=M
+    @iterator
+    A=M
+    M=D
+
+(DECREMENT_ITERATOR)
+    @iterator
+    M=M-1
+
+(LOOP_ITERATION)
+    @ITERATE
+    0;JMP
+(END_ITERATION)
+
+(END)
+    @START
+    0;JMP
