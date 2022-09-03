9f06769b2e5d7291fe4b7b5911a4592e4b29248d
kopackiw <26244440+kopackiw@users.noreply.github.com>
2022-09-03 23:19:18 +0200

    create assembler with label parsing

--- projects/06/hackAssembler/main.py
+++ projects/06/hackAssembler/main.py
@@ -53,9 +53,54 @@ def value_to_machine_code(value):
         case 'D|A' | 'D|M': return '010101'
 
 
+def substitute_labels_refs(line: str, labels, counter) -> str:
+    if not line.startswith('@'):
+        return line
+    elif line.startswith('@R') and line[2].isnumeric():
+        return '@' + line[2:]
+    elif line[1:].isnumeric():
+        return line
+    elif labels.get(line[1:]) is not None:
+        return '@' + str(labels.get(line[1:]))
+    else:
+        newValue = counter['v']
+        labels[line[1:]] = newValue
+        counter['v'] = counter['v'] + 1
+        return '@' + str(newValue)
+
+
+def resolve_labels(source_code: str) -> str:
+    variable_register_counter = {'v': 16}
+
+    labels = (
+        seq(lines(source_code))
+            .enumerate(start=1)
+            .filter(lambda tup: tup[1].startswith('('))
+            .map(lambda tup: (tup[1][1:-1], tup[0]))
+            .enumerate(start=1)
+            .map(lambda posTup: (posTup[1][0], posTup[1][1] - posTup[0]))
+            .to_dict()
+    )
+
+    # predefined labels
+    labels['SP'] = 0
+    labels['LCL'] = 1
+    labels['ARG'] = 2
+    labels['THIS'] = 3
+    labels['THAT'] = 4
+
+    return (
+        seq(lines(source_code))
+            .filter_not(lambda line: line.startswith('('))
+            .map(lambda a: substitute_labels_refs(a, labels, variable_register_counter))
+            .make_string('\n')
+    )
+
+
 def command_to_machine_code(command):
     if command['type'] == 'A':
         return format(int(command['address']), '016b')
+
     elif command['type'] == 'C':
         opcode = '1'
         fill = '11'
@@ -76,20 +121,27 @@ def command_to_machine_code(command):
             'JLE': '110',
             'JMP': '111',
         }[command['jump']]
-        
+
         return f'{opcode}{fill}{memory}{value}{dest}{jump}'
 
 
-to_machine_code = lambda line: command_to_machine_code(parse_to_command(line))
-
 def assembly(source_code: str) -> str:
-    return (
+    clean_source_with_labels = (
         seq(lines(source_code))
             .map(clean_whitespace)
             .filter(lambda line: line.strip() != '')
-            .map(to_machine_code)
+            .make_string('\n')
+    )
+
+    section_lookup_table = resolve_labels(clean_source_with_labels)
+
+    return (
+        seq(lines(section_lookup_table))
+            .map(parse_to_command)
+            .map(command_to_machine_code)
             .make_string('\n')
     ) + '\n'
+
 
 def main():
     path_to_asm = sys.argv[1]
@@ -97,7 +149,7 @@ def main():
     
     with open(path_to_asm, 'r') as source_file:
         with open(output_path, 'w') as output_file:
-            output_file.write((assembly(source_file.read())))
+            output_file.write(assembly(source_file.read()))
 
 
 if __name__ == '__main__':
